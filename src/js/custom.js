59885d888cc37b0fd2ba91bd9fe4bfa1e3a7d639
mohamad-gandomi <m.gandomi1995@gmail.com>
2023-02-23 19:11:19 +0330

    add single course page

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,6 +4,7 @@
 //
 //    1. Toggle Active Class Btn Group
 //    2. Countdown Timer
+//    2. Tab Content
 //
 //---------------------------------------------
 
@@ -62,4 +63,27 @@ document.addEventListener('readystatechange', event => {
           }
       }, 1000);
   }
+});
+
+//---------------------------------------------
+// 2. Tab Content
+//---------------------------------------------
+
+$(function () {
+
+  var activeIndex = $('.active-tab').index(),
+      $contentlis = $('.tabs-content li'),
+      $tabslis = $('.tabs li');
+  
+  // Show content of active tab on loads
+  $contentlis.eq(activeIndex).show();
+
+  $('.tabs').on('click', 'li', function (e) {
+    var $current = $(e.currentTarget),
+        index = $current.index();
+    
+    $tabslis.removeClass('active-tab');
+    $current.addClass('active-tab');
+    $contentlis.hide().eq(index).show();
+     });
 });
